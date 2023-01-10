b4ad8166fba6530f2f9eee751ddf42654a3f97d1
rajukumarazh <rajukumarazh@gmail.com>
2023-01-10 17:04:23 +0530

    login_vaidating

--- QuizzServer/server.js
+++ QuizzServer/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const student = require('./student');
 const dotenv = require('dotenv');
-// dotenv.config({ path: __dirname + '/.env' });
 const app = express();
-
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 const uri =
 	'mongodb+srv://raju:Ra%409058837496@cluster0.kjkyk5j.mongodb.net/cluster0?retryWrites=true&w=majority';
 // mongodb+srv://raju:<password>@cluster0.uwtgdvq.mongodb.net/test
@@ -13,21 +14,42 @@ const options = {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 };
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(
+	new LocalStrategy(function (email, password, done) {
+		user
+			.findOne({ email: username })
+			.then((user) => {
+				if (!user) {
+					return done(null, false, { message: 'user incorrect' });
+				}
+				if (!user.password == password) {
+					return done(null, false, { message: 'password incorrect' });
+				}
+				return done(null, user);
 
+				// Function defined at bottom of app.js
+				const isValid = validPassword(password, user.hash, user.salt);
+
+				if (isValid) {
+					return cb(null, user);
+				} else {
+					return cb(null, false);
+				}
+			})
+			.catch((err) => {
+				cb(err);
+			});
+	})
+);
 mongoose.connect(uri, options).then(() => {
 	console.log('database connnected');
 });
 app.get('/user', async (req, res) => {
-	// const data = await user.find();
-	// res.send(data);
-	// console.log('data', data);
-	const data = new student({
-		email: 'req.body.email',
-		password: 124536,
-	});
-	let rest = data.save();
-	res.send(rest);
+	res.send({ response: 'all fine' });
 });
 // const app=expres();
+//passport check
 
 app.listen(8000, () => console.log('server running at 8000'));
