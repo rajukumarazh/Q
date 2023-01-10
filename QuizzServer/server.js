c7abbc752212b755e2b95949235724080fadac89
rajukumarazh <rajukumarazh@gmail.com>
2023-01-10 12:46:46 +0530

    passport added

--- QuizzServer/server.js
+++ QuizzServer/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const user = require('./user');
+const student = require('./student');
 const dotenv = require('dotenv');
 // dotenv.config({ path: __dirname + '/.env' });
 const app = express();
-console.log('url', process.env);
+
 const uri =
-	'mongodb+srv://raju:Ra%409058837496@crud.kjkyk5j.mongodb.net/cluster0?retryWrites=true&w=majority';
+	'mongodb+srv://raju:Ra%409058837496@cluster0.kjkyk5j.mongodb.net/cluster0?retryWrites=true&w=majority';
+// mongodb+srv://raju:<password>@cluster0.uwtgdvq.mongodb.net/test
+// mongodb+srv://raju:<password>@cluster0.uwtgdvq.mongodb.net/test
 const options = {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
@@ -16,9 +18,15 @@ mongoose.connect(uri, options).then(() => {
 	console.log('database connnected');
 });
 app.get('/user', async (req, res) => {
-	const data = await user.find();
-	res.send(data);
-	console.log('data', data);
+	// const data = await user.find();
+	// res.send(data);
+	// console.log('data', data);
+	const data = new student({
+		email: 'req.body.email',
+		password: 124536,
+	});
+	let rest = data.save();
+	res.send(rest);
 });
 // const app=expres();
 
