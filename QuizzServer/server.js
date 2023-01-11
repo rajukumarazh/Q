1a6a715f939e159d4760d968ab5df719a12ea7dd
rajukumarazh <rajukumarazh@gmail.com>
2023-01-11 07:20:54 +0530

    updated

--- QuizzServer/server.js
+++ QuizzServer/server.js
@@ -6,6 +6,10 @@ const app = express();
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const session = require('express-session');
+const bodyParser=require("body-parser")
+app.use(bodyParser.json());
+// After you declare "app"
+app.use(session({ secret: 'melody hensley is my spirit animal' }));
 const uri =
 	'mongodb+srv://raju:Ra%409058837496@cluster0.kjkyk5j.mongodb.net/cluster0?retryWrites=true&w=majority';
 // mongodb+srv://raju:<password>@cluster0.uwtgdvq.mongodb.net/test
@@ -16,39 +20,45 @@ const options = {
 };
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(
-	new LocalStrategy(function (email, password, done) {
-		user
-			.findOne({ email: username })
-			.then((user) => {
-				if (!user) {
-					return done(null, false, { message: 'user incorrect' });
-				}
-				if (!user.password == password) {
-					return done(null, false, { message: 'password incorrect' });
-				}
-				return done(null, user);
-
-				// Function defined at bottom of app.js
-				const isValid = validPassword(password, user.hash, user.salt);
-
-				if (isValid) {
-					return cb(null, user);
-				} else {
-					return cb(null, false);
-				}
-			})
-			.catch((err) => {
-				cb(err);
-			});
-	})
-);
+passport.use(new LocalStrategy(
+  function(email, password, done) {
+	  console.log(email,password)
+    student.findOne({ email: email }, function (err, std) {
+      if (err) { return done(err); }
+      if (!std) { return done(null, false); }
+      if (!std.password==password) { return done(null, false); }
+      return done(null, std);
+    });
+  }
+));
+app.post('/login', 
+  passport.authenticate('local'),
+  function(req, res) {
+	  console.log("hello")
+   res.send(res)
+  });
+  passport.serializeUser((std,done)=>{
+	  if(std){
+		  return done(null,user.id)
+	  }
+	  return done(null,std.id)
+	 
+  })
+  passport.deserializeUser((id,done)=>{
+	  student.findById(id,(err,std)=>{
+		  if(err){
+			  return done(null,false)
+		  }
+		  return done(null,std)
+	  })
+  })
 mongoose.connect(uri, options).then(() => {
 	console.log('database connnected');
 });
 app.get('/user', async (req, res) => {
 	res.send({ response: 'all fine' });
 });
+
 // const app=expres();
 //passport check
 
