bebbef52cf59f9db2e31c6814e354c81e409f8bc
rajukumarazh <rajukumarazh@gmail.com>
2023-02-05 11:25:37 +0530

    comment_removed

--- Quizz/src/Redux/Toolkit/rootSlice.js
+++ Quizz/src/Redux/Toolkit/rootSlice.js
@@ -1,6 +1,5 @@
 import { combineReducers } from '@reduxjs/toolkit';
-// import ApiDataSlice from './ApiDataSlice';
-// import counterSlice from './counterSlice';
+
 import CourseSlice from './CourseSlice';
 export const rootslice = combineReducers({
 	levelUp: CourseSlice,
