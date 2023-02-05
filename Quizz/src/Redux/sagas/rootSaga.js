25cdb448536116ad188055ffacaa98e66abdf1cb
rajukumarazh <rajukumarazh@gmail.com>
2023-02-05 11:31:19 +0530

    develop_created

--- Quizz/src/Redux/sagas/rootSaga.js
+++ Quizz/src/Redux/sagas/rootSaga.js
@@ -1,12 +1,9 @@
 import { takeLatest, takeEvery } from 'redux-saga/effects';
-// import { handleapi } from './Handler/HandleGetApi';
-// import { handleapi2 } from './Handler/HandleGetApi2';
+
 import { handleCourseApi } from './handlers/HandleCourse';
 import { all, fork } from 'redux-saga/effects';
 export function* watcherSaga() {
 	// yield takeLatest(GET_USER, handleGetUser);
-	// yield takeLatest(GET_TODO, manageTodos);
-	// yield takeEvery('counter/calling', handleapi);
-	// yield takeLatest('apilice/fetchApiData', handleapi2);
+	
 	yield all([fork(handleCourseApi)]);
 }
